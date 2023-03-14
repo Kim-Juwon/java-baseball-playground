7f881f46202b024ca73856d4341374a990b18181
김주원 <damiano102777@naver.com>
2023-03-14 23:12:49 +0900

    String 클래스에 대한 학습 테스트 - 요구사항1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,13 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] actual1 = "1,2".split(",");
+        String[] actual2 = "1".split(",");
+
+        assertThat(actual1).containsExactly("1", "2");
+        assertThat(actual2).contains("1");
+    }
 }
