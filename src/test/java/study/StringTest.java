b186bfcdfcbdcea1b4b1df047a8896b412768b24
김주원 <damiano102777@naver.com>
2023-03-15 12:35:08 +0900

    String 클래스에 대한 학습 테스트 - 요구사항3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -26,5 +28,22 @@ public class StringTest {
         String actual = input.substring(1, input.length() - 1);
 
         assertThat(actual).isEqualTo("1, 2");
+    }
+
+    @Test
+    @DisplayName("charAt() 메소드가 특정 위치의 문자를 정확히 가져오는지 확인")
+    void charAt() {
+        char actual = "abc".charAt(0);
+
+        assertThat(actual).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt() 메소드 사용시 위치값을 벗어났을때 StringIndexOutOfBoundsException이 발생하는지 확인")
+    void stringIndexOutBoundsExceptionForCharAt() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char ch = "abc".charAt(3);
+                });
     }
 }
