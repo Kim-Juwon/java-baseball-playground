f5e5ffc5384fcc1eed7003c9a67d253a40cbf136
김주원 <damiano102777@naver.com>
2023-03-18 16:40:35 +0900

    feat: main 메소드에서 게임 실행하는 코드 작성

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.game.BaseballGame;
+
 public class Main {
     public static void main(String[] args) {
+        BaseballGame baseballGame = new BaseballGame();
 
+        baseballGame.run();
     }
 }
