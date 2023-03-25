85c31e05b1bb0bfb8fd328b01bb60cbdd22c8ba8
김주원 <damiano102777@naver.com>
2023-03-25 15:58:40 +0900

    feat: component 수정에 따른 BaseballGame 코드 수정

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -2,61 +2,109 @@ package baseball.game;
 
 import baseball.component.Hint;
 import baseball.component.Numbers;
-import baseball.component.RandomNumberGenerator;
+import baseball.component.RandomNumbersGenerator;
+import baseball.component.FinishingStatus;
+import baseball.exception.base.NonCriticalException;
 import baseball.view.InputView;
 import baseball.view.ResultView;
 
 public class BaseballGame {
-    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-    private final InputView inputView = new InputView();
-    private final ResultView resultView = new ResultView();
-    private static final int RESTART = 1;
-    private static final int EXIT = 2;
+    private final RandomNumbersGenerator randomNumbersGenerator = RandomNumbersGenerator.create();
+    private final InputView inputView = InputView.create();
+    private final ResultView resultView = ResultView.create();
     private boolean playing;
-    private Numbers correctNumber;
-    private Numbers inputNumber;
-    private Hint hints;
+    private Numbers correctNumbers;
+    private Numbers inputNumbers;
+    private Hint hint;
 
     public void run() {
-        setUp();
+        start();
 
-        while (playing) {
-            inputView.showInputView();
-            inputNumber = inputView.inputAndReturn();
+        while (isPlaying()) {
+            try {
+                initialize();
 
-            hints = correctNumber.compareAndGetHints(inputNumber);
+                // 숫자 입력
+                inputView.showInputMessage();
+                inputNumbers = inputView.input();
 
-            boolean success = resultView.showResultAndGetSuccessStatus(hints);
-            showQuestionWhenSuccessful(success);
+                // 힌트 도출
+                hint = correctNumbers.compare(inputNumbers);
+
+                // 힌트를 통해 결과 출력
+                resultView.showResultFrom(hint);
+
+                // 성공했다면 재시작할것인지 확인
+                showRestartQuestionViewIfSuccessful();
+            } catch (Throwable throwable) {
+                resultView.showErrorFrom(throwable);
+                terminateWhenUnprotectedErrorOccurs(throwable);
+            }
         }
     }
 
-    private void setUp() {
-        playing = true;
-        generateRandomNumber();
+    private boolean isPlaying() {
+        return playing;
     }
 
-    private void exit() {
+    private void initialize() {
+        emptyInputNumbers();
+        emptyHint();
+    }
+
+    private void start() {
+        playing = true;
+        generateRandomCorrectNumbers();
+    }
+
+    private void terminate() {
         playing = false;
     }
 
-    private void generateRandomNumber() {
-        correctNumber = randomNumberGenerator.generateNumber();
+    private void generateRandomCorrectNumbers() {
+        correctNumbers = randomNumbersGenerator.generateNumbers();
     }
 
-    private void showQuestionWhenSuccessful(boolean success) {
-        if (!success) {
+    private void emptyInputNumbers() {
+        inputNumbers = null;
+    }
+
+    private void emptyHint() {
+        hint = null;
+    }
+
+    private boolean isSuccessful() {
+        if (hint == null) {
+            return false;
+        }
+
+        return hint.isThreeStrike();
+    }
+
+    private void showRestartQuestionViewIfSuccessful() {
+        if (!isSuccessful()) {
             return;
         }
 
-        resultView.showRestartOrExitView();
-        int restartOrExit = resultView.inputAndReturnForRestartOrExit();
+        resultView.showQuestion();
+        decideRestartOrTerminateFrom(resultView.input());
+    }
 
-        if (restartOrExit == RESTART) {
-            setUp();
+    private void decideRestartOrTerminateFrom(FinishingStatus status) {
+        if (status.equals(FinishingStatus.RESTART)) {
+            generateRandomCorrectNumbers();
+            initialize();
         }
-        if (restartOrExit == EXIT) {
-            exit();
+        if (status.equals(FinishingStatus.TERMINATE)) {
+            terminate();
         }
+    }
+
+    private void terminateWhenUnprotectedErrorOccurs(Throwable throwable) {
+        if (throwable instanceof NonCriticalException) {
+            return;
+        }
+
+        terminate();
     }
 }
