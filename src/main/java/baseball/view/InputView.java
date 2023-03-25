79967de23b3122fd8e7d7461e86156010741c4e7
김주원 <damiano102777@naver.com>
2023-03-25 15:56:56 +0900

    feat: 입력 값에 대한 예외 처리 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,18 +1,109 @@
 package baseball.view;
 
 import baseball.component.Numbers;
+import baseball.exception.message.ExceptionMessage;
+import baseball.exception.noncritical.IllegalInputException;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class InputView {
-    private final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String WHITESPACE_REGEX = "\\s+";
+    private static final int CORRECT_NUMBERS_LENGTH = 3;
+    private static final int CORRECT_STRINGS_LENGTH = 1;
+    private final Scanner scanner = new Scanner(System.in);
 
-    public void showInputView() {
+    public static InputView create() {
+        return new InputView();
+    }
+
+    public void showInputMessage() {
         System.out.print(INPUT_MESSAGE);
     }
 
-    public Numbers inputAndReturn() {
-        return Numbers.from(scanner.nextLine());
+    public Numbers input() {
+        return Numbers.from(read());
+    }
+
+    private String read() {
+        String string = readFirstStringFrom(readStringsByRemovingWhitespaces());
+        validate(string);
+        return string;
+    }
+
+    private String readFirstStringFrom(String[] strings) {
+        validate(strings);
+        return strings[0];
+    }
+
+    private String[] readStringsByRemovingWhitespaces() {
+        return scanner.nextLine()
+                .trim()
+                .split(WHITESPACE_REGEX);
+    }
+
+    // 아래부터는 입력값에 대한 유효성 검증을 위한 메소드이다.
+
+    private boolean isNull(Object object) {
+        return object == null;
+    }
+
+    private void validate(String string) {
+        validateCorrectLength(string);
+        validateBetweenOneAndNine(string);
+        validateNonDuplication(string);
+    }
+
+    private void validate(String[] strings) {
+        validateCorrectLength(strings);
+        validateFirstValueExist(strings);
+    }
+
+    private void validateCorrectLength(String string) {
+        if (isNull(string) || string.length() != CORRECT_NUMBERS_LENGTH) {
+            throw new IllegalInputException(ExceptionMessage.NUMBERS_LENGTH_MUST_BE_THREE);
+        }
+    }
+
+    private void validateBetweenOneAndNine(String string) {
+        if (isNull(string)) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
+
+        IntStream.range(0, string.length())
+                .filter(index -> !isBetweenOneAndNine(string.charAt(index)))
+                .forEach(index -> {
+                    throw new IllegalInputException(ExceptionMessage.NUMBER_MUST_BE_BETWEEN_ONE_AND_NINE);
+                });
+    }
+
+    private void validateNonDuplication(String string) {
+        if (string.length() != getDeduplicatedLengthFrom(string)) {
+            throw new IllegalInputException(ExceptionMessage.DUPLICATE_NUMBER_EXIST);
+        }
+    }
+
+    private long getDeduplicatedLengthFrom(String string) {
+        return IntStream.range(0, string.length())
+                .map(string::charAt)
+                .distinct()
+                .count();
+    }
+
+    private boolean isBetweenOneAndNine(char ch) {
+        return ch >= '1' && ch <= '9';
+    }
+
+    private void validateCorrectLength(String[] strings) {
+        if (isNull(strings) || strings.length != CORRECT_STRINGS_LENGTH) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
+    }
+
+    private void validateFirstValueExist(String[] strings) {
+        if (isNull(strings) || isNull(strings[0])) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
     }
 }
--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -1,61 +1,122 @@
 package baseball.view;
 
 import baseball.component.Hint;
-import baseball.component.Status;
+import baseball.component.FinishingStatus;
+import baseball.exception.message.ExceptionMessage;
+import baseball.exception.base.NonCriticalException;
+import baseball.exception.noncritical.IllegalInputException;
 
 import java.util.Scanner;
 
 public class ResultView {
-    private static final String MESSAGE_FOR_SUCCESS = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String MESSAGE_FOR_QUESTION_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String WHITESPACE_REGEX = "\\s+";
+    private static final String UNEXPECTED_ERROR_MESSAGE = "예기치 못한 에러가 발생하였습니다.";
     private final Scanner scanner = new Scanner(System.in);
 
-    public boolean showResultAndGetSuccessStatus(Hint hints) {
-        if (isSuccess(hints)) {
+    public static ResultView create() {
+        return new ResultView();
+    }
+
+    public void showResultFrom(Hint hint) {
+        if (isSuccess(hint)) {
             showSuccessView();
-            return true;
-        }
-
-        showHintsView(hints);
-        return false;
-    }
-
-    private boolean isSuccess(Hint hints) {
-        return hints.hasOnlyStrike() && hints.isSizeOfStrikeEqualToFullStrike();
-    }
-
-    private void showSuccessView() {
-        System.out.printf(MESSAGE_FOR_SUCCESS, Hint.SIZE_OF_FULL_STRIKE);
-    }
-
-    public void showRestartOrExitView() {
-        System.out.println(MESSAGE_FOR_QUESTION_RESTART);
-    }
-
-    public int inputAndReturnForRestartOrExit() {
-        return scanner.nextInt();
-    }
-
-    private void showHintsView(Hint hints) {
-        if (hints.isNothing()) {
-            System.out.println(Status.NOTHING.getMeaning());
             return;
         }
 
-        System.out.println(makeStringOfHints(hints));
+        showHintFrom(hint);
     }
 
-    private String makeStringOfHint(Hint hint) {
-        return String.format("%d%s", hint.getCount(), hint.getStatus().getMeaning());
+    public void showQuestion() {
+        System.out.println(MESSAGE_FOR_QUESTION_RESTART);
     }
 
-    private String makeStringOfHints(Hint hints) {
-        StringBuilder stringBuilder = new StringBuilder();
+    public void showErrorFrom(Throwable throwable) {
+        if (throwable instanceof NonCriticalException) {
+            System.out.println(throwable.getMessage());
+            return;
+        }
 
-        hints.getHintList().forEach(hint -> {
-            stringBuilder.append(makeStringOfHint(hint)).append(" ");
-        });
+        System.out.println(UNEXPECTED_ERROR_MESSAGE);
+    }
 
-        return stringBuilder.toString();
+    private boolean isSuccess(Hint hint) {
+        return hint.isThreeStrike();
+    }
+
+    private void showSuccessView() {
+        System.out.println(SUCCESS_MESSAGE);
+    }
+
+    private void showHintFrom(Hint hint) {
+        System.out.println(hint.makeString());
+    }
+
+    public FinishingStatus input() {
+        return FinishingStatus.from(read());
+    }
+
+    private int read() {
+        String string = readFirstStringFrom(readStringsByRemovingWhitespaces());
+        int intValue = parseToInt(string);
+        validate(intValue);
+        return intValue;
+    }
+
+    private String readFirstStringFrom(String[] strings) {
+        validate(strings);
+        return strings[0];
+    }
+
+    private String[] readStringsByRemovingWhitespaces() {
+        return scanner.nextLine()
+                .trim()
+                .split(WHITESPACE_REGEX);
+    }
+
+    private int parseToInt(String string) {
+        if (isNull(string)) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
+
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
+    }
+
+    // 아래부터는 입력값에 대한 유효성 검증을 위한 메소드들이다.
+
+    private boolean isNull(Object object) {
+        return object == null;
+    }
+
+    private void validate(int intValue) {
+        validateAnyMatches(intValue);
+    }
+
+    private void validate(String[] strings) {
+        validateCorrectLength(strings);
+        validateFirstValueExist(strings);
+    }
+
+    private void validateAnyMatches(int intValue) {
+        if (!FinishingStatus.anyMatch(intValue)) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
+    }
+
+    private void validateCorrectLength(String[] strings) {
+        if (isNull(strings) || strings.length != 1) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
+    }
+
+    private void validateFirstValueExist(String[] strings) {
+        if (isNull(strings) || strings[0] == null) {
+            throw new IllegalInputException(ExceptionMessage.INPUT_VALUE_IS_INVALID);
+        }
     }
 }
